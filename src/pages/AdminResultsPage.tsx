0f773b9f72181b88f0c9feb8563a20c1d45cb79f
[dyad] <dr.tamnguyen@hotmail.com>
2025-08-14 21:41:43 +0700

    [dyad] Fixed unused imports in AdminResultsPage - wrote 1 file(s)

--- src/pages/AdminResultsPage.tsx
+++ src/pages/AdminResultsPage.tsx
@@ -12,9 +12,7 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { format } from "date-fns";
